27333c42461fc43cf8e1b5eab424e6b98254a17c
Your Name <ferdousjoya>
2021-10-25 17:17:06 -0700

    handle usecase unix

--- server.js
+++ server.js
@@ -34,11 +34,22 @@ app.get("/api/timestamp", function(req, res) {
   });
 });
 
-let responseObject = {};
+
 app.get("/api/timestamp/:date_string", function (req, res) {
   let dateString = req.params.date_string
   let passedInValue = new Date(dateString);
   
+
+if (parseInt(dateString) > 10000) {
+  let unixTime = new Date(parseInt(dateString));
+res.json({
+    "unix": unixTime.getTime(),
+    "utc": unixTime.toUTCString()
+  });
+}
+
+
+
 if (passedInValue == "Invalid Date") {
 res.json({"error": "Invalid Date"});
 } else {
