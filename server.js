33bf6ad8d8f925f4afa5045499d48dff626a6d78
Your Name <ferdousjoya>
2021-10-26 12:52:40 -0700

    add mongoose mongodb body parser and short id to package update server js

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
 // server.js
 // where your node app starts
-
+var database_uri = "mongodb+srv://fredjoya:mongoose@cluster0.3tamr.mongodb.net/Cluster0?retryWrites=true&w=majority";
 // init project
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
+var bodyParser = require("body-parser");
+var mongo = require("mongodb");
+var mongoose = require("mongoose");
+var shortid = require("shortid");
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
 const { response } = require('express');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+
+//mongoose.connect(process.env.DB_URI)
+mongoose.connect(database_uri, {
+  useNewUrlParser: true, 
+  useUnifiedTopology: true
+});
+
+
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -20,6 +33,10 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+
+app.get("/URLshortener", function (req, res) {
+  res.sendFile(__dirname + '/views/URLshortener.html');
+});
 
 let responseObject = {}
 app.enable("trust proxy")
@@ -73,7 +90,53 @@ res.json({"error": "Invalid Date"});
 }
 });
 
+ 
+var ShortURL = mongoose.model('ShortURL', new mongoose.Schema({ 
+  short_url: String,
+  original_url: String,
+  suffix: String
+}));
 
+app.use(bodyParser.urlencoded({ extended: false}))
+
+app.use(bodyParser.json())
+
+
+  app.post("/api/shorturl", function (req, res) {
+    
+    let client_req_url = req.body.url
+    let suffix = shortid.generate();
+    let newShortURL = suffix
+    console.log(suffix, "this will be suffix");
+    let newURL = ShortURL({
+      short_url: __dirname + "/api/shorturl/" + suffix,
+      original_url: client_req_url,
+      suffix: suffix
+    })
+    newURL.save(function(err, doc) {
+    if (err) return console.error(err);
+    res.json({
+    "saved": true,
+    "short_url": newURL.short_url,
+    "original_url": newURL.original_url,
+    "suffix": newURL.suffix
+    });
+  });
+});
+
+app.get("/api/shorturl/:suffix", function (req, res) {
+  let userGenSuffix = req.params.suffix;
+  ShortURL.find({suffix: userGenSuffix}).then(function(foundUrls) {
+  let urlForRedirect = foundUrls[0];
+  
+  res.redirect(urlForRedirect.original_url);
+  });
+    // res.json({
+    //   "userGenSuffix": userGenSuffix,
+    //   "userReqUrl": userReqUrl
+    // })
+  });
+ 
 
 // listen for requests :)
 var listener = app.listen(port, function () {
